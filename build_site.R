2025eb2a569debe3dc3eb454e02633ef58127a3c
atrembla <54289193+atrembla@users.noreply.github.com>
2021-05-21 13:58:32 -0700

    update

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/kuhllabmanager/Desktop/atrembla.github.io")
+setwd("/Users/atrembla/Desktop/atrembla.github.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
